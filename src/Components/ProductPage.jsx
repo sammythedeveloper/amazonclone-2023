c0db671df9d7c50be4c4a1e1338b7860cb6cf649
sammythedeveloper <justsammy484@gmail.com>
2025-10-26 23:32:26 -0400

    grid

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -70,7 +70,7 @@ const ProductPage = () => {
       )}
 
       {/* Product Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
         {categoryData.map((item) => (
           <ProductDetail
             key={item.id}
